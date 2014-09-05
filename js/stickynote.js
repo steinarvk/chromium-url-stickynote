b85b9d4e3eed8d338c8666ef6de4961dc37dc959
Steinar V. Kaldager <steinarvkaldager@gmail.com>
2014-09-05 19:24:38 +0900

    add jquery and a few buttons

--- js/stickynote.js
+++ js/stickynote.js
@@ -1,8 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var editor = document.getElementById("stickynote-editor");
-    if(!editor) {
-        return;
+function setupStickyNote(url, ui) {
+    var originalText = "URL: " + url;
+
+    function onReset() {
+        ui.editor.val(originalText);
     }
 
-    editor.value = "Hello world: " + Math.random();
+    function onSave() {
+        originalText = ui.editor.val();
+    }
+
+    ui.editor.val(originalText);
+    ui.reset.click(onReset);
+    ui.save.click(onSave);
+};
+
+function grabUi() {
+    return {
+        editor: $("#stickynote-editor"),
+        reset: $("#stickynote-reset"),
+        save: $("#stickynote-save")
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    chrome.tabs.getSelected(function(tab) {
+        setupStickyNote(tab.url, grabUi());
+    });
 });
